f4d3ca6c20408573b6c26641bc912612021470bd
Janice-M <janiceink001@gmail.com>
2019-07-23 10:15:11 +0300

    the debug = true is being handeled at the config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from .config import DevConfig
 #initializing application
 app = Flask(__name__)
+#Setting up configuration
+app.config.from_object(DevConfig)
 from app import views
