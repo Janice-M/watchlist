4391a86935b2685cc8f59ce15a624e0c23485a36
Janice-M <janiceink001@gmail.com>
2019-07-28 15:17:41 +0300

    working but okay

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,14 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_movies,get_movie,search_movie
-from .models import reviews
+from . import main
+from ..request import get_movies,get_movie,search_movie
+from ..models import Review
 from .forms import ReviewForm
 
-Review = reviews.Review
+
 
 
 #views 
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -24,11 +24,11 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name = search_movie))
+        return redirect(url_for('main.search',movie_name = search_movie))
     else:
         return render_template('index.html',title = title, popular = popular_movies,upcoming = upcoming_movie, now_playing = now_showing_movie)
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
     '''
     View root page function theat returns the index pages and its  data 
@@ -38,7 +38,7 @@ def movie(id):
     reviews = Review.get_reviews(movie.id)
     return render_template('movie.html',  title = title, movie = movie, reviews = reviews)
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     view function to display search results
@@ -50,7 +50,7 @@ def search(movie_name):
     return render_template('search.html', title = title, movies = searched_movies)
 
 
-@app.route('/movie/review/new/<int:id>', methods= ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods= ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
---
+++ start.sh
@@ -0,0 +1,5 @@
+
+export MOVIE_API_KEY='57f37baa4a0f7353fb23a56f3d599e38'
+export SECRET_KEY='wamlambez69'
+
+python3.6 manage.py server
