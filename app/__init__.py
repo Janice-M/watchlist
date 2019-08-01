3099671c2eca2316c722ec691d7b1bf08208c2df
Janice-M <janiceink001@gmail.com>
2019-08-01 09:15:27 +0300

    updated login

--- app/__init__.py
+++ app/__init__.py
@@ -31,8 +31,8 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+    from . auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     
     # setting config
     from .request import configure_request
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,9 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
-from ..models import User
-from .forms import LoginForm,RegistrationForm
 
 
 @auth.route('/login',methods = ['GET',"POST"])
@@ -35,6 +33,6 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
-        title = "New Account"
+        # title = "New Account"
     return render_template('auth/register.html', registration_form = form)
 
